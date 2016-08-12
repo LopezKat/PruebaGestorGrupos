3a80bb936b47e0fea6ceee8e294356353bb0b304
kathe <kathe@DESKTOP-7IB108C>
2016-08-11 21:28:17 -0500

    SE AGREGARON LOS CAMPOS DE FECHA DE BAJA Y ESTADO, LO CUAL SE REQUIERE EN LAS DEMAS CLASES.

--- GestorGrupos_DTO/src/co/edu/uniremington/gestorgrupos/dto/TipoProgramaDTO.java
+++ GestorGrupos_DTO/src/co/edu/uniremington/gestorgrupos/dto/TipoProgramaDTO.java
@@ -1,8 +1,15 @@
 package co.edu.uniremington.gestorgrupos.dto;
 
+import java.util.Date;
+
 public class TipoProgramaDTO {
 	private int codigo;
 	private String nombre;
+
+	// Agregar estos dos campos en todas las clases y aplicar el GET y el SET;
+	// ademas hacer la tabla Grupos
+	private Date fecha_baja;
+	private String estado;
 
 	public int getCodigo() {
 		return codigo;
@@ -18,5 +25,21 @@ public class TipoProgramaDTO {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public Date getFecha_baja() {
+		return fecha_baja;
+	}
+
+	public void setFecha_baja(Date fecha_baja) {
+		this.fecha_baja = fecha_baja;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
 	}
 }
